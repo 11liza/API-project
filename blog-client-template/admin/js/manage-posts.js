8f371c214f4fb52590c752ba27ad3b06cd438c7d
huenguyensw <hue.blue@gmail.com>
2022-12-13 13:06:23 +0100

    commit

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -8,72 +8,108 @@ fetch('https://blog-api-assignment.up.railway.app/posts')
     })
     .then((data) => {
         let HTMLContent = "";
-        for (let post of data) {
-            // format Tags field
-            let tagContent = "";
-            if (post.tags) {
-                for (let tag of post.tags) {
-                    tagContent += tag + ', ';
+        const pageIndex = 1;
+        const paginationLimit = 10;
+        const tbody = document.querySelector('tbody');
+        
+        if (!data) {
+            const h3 = document.createElement('h3');
+            h3.innerHTML = 'No data';
+            tbody.appendChild(h3);
+        } else {
+            tbody.innerHTML = "";
+            for (let post of data) {
+                // format Tags field
+                let tagContent = "";
+                if (post.tags) {
+                    for (let tag of post.tags) {
+                        tagContent += tag + ', ';
+                    }
                 }
+                tagContent = tagContent.slice(0, tagContent.length - 2);
+
+                // format Date field
+                const dateValue = new Date(post.date);
+                HTMLContent +=
+                    `<tr>
+                <td>${post.title}</td>
+                <td>${post.author}</td>
+                <td>${tagContent}</td>
+                <td>${dateValue.getFullYear()}-${dateValue.getMonth()}-${dateValue.getDay()} ${dateValue.getHours()}:${dateValue.getMinutes()}</td>
+                <td>
+                    <a href="update-post.html?id=${post._id}">Update</a> |
+                    <a href="#" data-id=${post._id}>Delete</a>
+                </td>
+                </tr>`
             }
-            tagContent = tagContent.slice(0, tagContent.length - 2);
+            document.getElementById('table').lastElementChild.innerHTML = HTMLContent;
+            //Delete post
+            let form = document.getElementById('form');
+            let delLinks = form.querySelectorAll("a[data-id]");
+            for (let link of delLinks) {
+                link.addEventListener('click', async (e) => {
+                    console.log(link.parentNode.parentNode)
+                    let id = e.target.dataset.id;
+                    try {
+                        await fetch('https://blog-api-assignment.up.railway.app/posts/' + id, {
+                            method: 'DELETE'
+                        })
+                        link.parentNode.parentNode.remove();
 
-            // format Date field
-            const dateValue = new Date(post.date);
-            HTMLContent +=
-                `<tr>
-            <td>${post.title}</td>
-            <td>${post.author}</td>
-            <td>${tagContent}</td>
-            <td>${dateValue.getFullYear()}-${dateValue.getMonth()}-${dateValue.getDay()} ${dateValue.getHours()}:${dateValue.getMinutes()}</td>
-            <td>
-                <a href="update-post.html?id=${post._id}">Update</a> |
-                <a href="#" data-id=${post._id}>Delete</a>
-            </td>
-            </tr>`
+                        // find current page
+                        console.log(document.querySelector('.active'));
+                        const pageIndex = Number(document.querySelector('.active').getAttribute('page-index'));
+                        console.log(pageIndex);
+                        // pagination efter deleting post
+                        fetch('https://blog-api-assignment.up.railway.app/posts')
+                            .then((response) => {
+                                return response.json();
+                            })
+                            .then((data) => {
+                                if (!data) {
+                                    const h3 = document.createElement('h3');
+                                    h3.innerHTML = 'No data';
+                                    document.querySelector('tbody').appendChild(h3)
+                                } else {
+                                    document.getElementById('pagination-numbers').innerHTML = "";
+                                    console.log(document.getElementById('pagination-numbers').childNodes)
+                                    pagination(data.length, paginationLimit, pageIndex);
+                                }
+
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            })
+                    } catch (error) {
+                        console.log(error);
+                    }
+                })
+            }
+
+            //pagination
+            pagination(data.length, paginationLimit, pageIndex)
         }
-        document.getElementById('table').lastElementChild.innerHTML = HTMLContent;
-
-        //Delete post
-        let form = document.getElementById('form');
-        let delLinks = form.querySelectorAll("a[data-id]");
-        for (let link of delLinks) {
-            link.addEventListener('click', async (e) => {
-                console.log(link.parentNode.parentNode)
-                let id = e.target.dataset.id;
-                try {
-                    await fetch('https://blog-api-assignment.up.railway.app/posts/' + id, {
-                        method: 'DELETE'
-                    })
-                    link.parentNode.parentNode.remove();
-                } catch (error) {
-                    console.log(error);
-                }
-            })
-        }
-
-        //pagination
-        pagination(data)
     })
     .catch((error) => {
         console.log(error);
     })
 
-function pagination(data) {
+function pagination(numberOfPosts, paginationLimit, currentPage) {
 
-    const numberOfPosts = data.length;
     const nextButton = document.getElementById("next-button");
     const prevButton = document.getElementById("prev-button");
     const paginationNumbers = document.getElementById('pagination-numbers');
     const body = document.querySelector('tbody');
     const rowList = body.querySelectorAll('tr');
     console.log(rowList);
-    console.log(numberOfPosts)
 
-
-    const paginationLimit = 10;
+    //calculate number of pages
     const pageCount = Math.ceil(numberOfPosts / paginationLimit);
-    let currentPage = 1;
+    console.log(pageCount);
+
+    if (currentPage > pageCount) {
+        currentPage = pageCount;
+    }
 
     const disableButton = (button) => {
         button.classList.add('disabled');
@@ -84,7 +120,7 @@ function pagination(data) {
         button.classList.remove("disabled");
         button.removeAttribute("disabled");
     }
-
+    //handle status of previous and next buttons
     const handlePageButtonsStatus = () => {
         if (currentPage == 1) {
             disableButton(prevButton)
@@ -100,13 +136,18 @@ function pagination(data) {
     }
 
     const handleActivePageNumber = () => {
-        console.log('handle')
-        document.querySelectorAll(".pagination-number").forEach((button) => {
+        console.log(document.getElementById('pagination-numbers').childNodes)
+        document.getElementById('pagination-numbers').childNodes.forEach((button) => {
+
             button.classList.remove('active');
+            button.style.backgroundColor = 'unset';
+            console.log(button)
             const pageIndex = Number(button.getAttribute("page-index"));
             console.log(pageIndex);
             if (pageIndex == currentPage) {
                 button.classList.add('active');
+                button.style.backgroundColor = 'grey';
+                console.log(button)
             }
         })
     }
@@ -119,7 +160,7 @@ function pagination(data) {
         pageNumber.setAttribute('arial-label', "Page " + index);
         paginationNumbers.appendChild(pageNumber);
     }
-
+    // generate page indexes
     const getPaginationNumbers = () => {
         for (let i = 1; i <= pageCount; i++) {
             appendPageNumber(i);
@@ -132,13 +173,13 @@ function pagination(data) {
         handleActivePageNumber();
         handlePageButtonsStatus();
 
-        
+        //calculate first och last post of one page
         const startPoint = (pageNum - 1) * paginationLimit;
         const endPoint = pageNum * paginationLimit;
 
         for (let i = 0; i < rowList.length; i++) {
             console.log(rowList[i]);
-            rowList[i].setAttribute("hidden","true");
+            rowList[i].setAttribute("hidden", "true");
             // show all posts of current page
             if (i >= startPoint && i < endPoint) {
                 rowList[i].removeAttribute("hidden");
@@ -146,30 +187,23 @@ function pagination(data) {
         }
     }
     getPaginationNumbers();
-    setCurrentPage(1);
-    // window.onload = function(){
-    //     console.log('co khog')
-    //     getPaginationNumbers();
-    //     setCurrentPage(1);
-    // }
+    setCurrentPage(currentPage);
+
+
     prevButton.addEventListener("click", () => {
-        console.log('iiii')
         setCurrentPage(currentPage - 1);
     });
 
     nextButton.addEventListener("click", () => {
         setCurrentPage(currentPage + 1);
-        console.log("aaaa")
     });
 
-    const availablePages =  paginationNumbers.childNodes;
-   for(let button of availablePages ){
-    const pageIndex = Number(button.getAttribute('page-index'));
-    console.log(pageIndex)
-    button.addEventListener("click", ()=>{
-        setCurrentPage(pageIndex);
-    })
-   }
+    const availablePages = paginationNumbers.childNodes;
+    for (let button of availablePages) {
+        const pageIndex = Number(button.getAttribute('page-index'));
+        console.log(pageIndex)
+        button.addEventListener("click", (e) => {
+            setCurrentPage(pageIndex);
+        })
+    }
 }
-
-
