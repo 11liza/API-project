e892b12bad2f33903993b5b968ef359db4b9d5ab
huenguyensw <hue.blue@gmail.com>
2022-12-13 18:04:52 +0100

    commit

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -9,22 +9,14 @@ fetch('https://blog-api-assignment.up.railway.app/posts')
     .then((data) => {
         let HTMLContent = "";
         for (let post of data) {
-            // format Tags field
-            let tagContent = "";
-            if (post.tags) {
-                for (let tag of post.tags) {
-                    tagContent += tag + ', ';
-                }
-            }
-            tagContent = tagContent.slice(0, tagContent.length - 2);
-            
             // format Date field
             const dateValue = new Date(post.date);
+
             HTMLContent +=
                 `<tr>
             <td>${post.title}</td>
             <td>${post.author}</td>
-            <td>${tagContent}</td>
+            <td>${post.tags}</td>
             <td>${dateValue.getFullYear()}-${dateValue.getMonth()}-${dateValue.getDay()} ${dateValue.getHours()}:${dateValue.getMinutes()}</td>
             <td>
                 <a href="update-post.html?id=${post._id}">Update</a> |
@@ -39,7 +31,6 @@ fetch('https://blog-api-assignment.up.railway.app/posts')
         let delLinks = form.querySelectorAll("a[data-id]");
         for (let link of delLinks) {
             link.addEventListener('click',async(e)=>{
-                console.log(link.parentNode.parentNode)
                 let id = e.target.dataset.id;
                 try{
                     await fetch('https://blog-api-assignment.up.railway.app/posts/'+id,{
